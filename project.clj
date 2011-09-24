092b2420f41ff3cd46054486ebeebab10f7cc2a8
Maximilian Weber <weber.maximilian@googlemail.com>
2011-09-24 16:43:31 +0200

    Project prefix

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
-(defproject org.jmatt/postal "1.6-SNAPSHOT"
-  :description "Fork of https://github.com/drewr/postal"
-  :url "https://github.com/jmatt/postal"
+(defproject org.clojars.maxweber/postal "1.6-SNAPSHOT"
   :resources-path "etc"
   :repositories {"java.net" "http://download.java.net/maven/2"
                  "clojars" "http://clojars.org/repo"}
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [javax.mail/mail "1.4.4"
                   :exclusions [javax.activation/activation]]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [lein-clojars "0.6.0"]])
+  :dev-dependencies [[lein-clojars "0.6.0"]
+                     [swank-clojure "1.4.0-SNAPSHOT"]])
